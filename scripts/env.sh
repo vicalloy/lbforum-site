8767c0298e8d241b2f8764e2aad757dc01bf5501
vicalloy <zbirder@gmail.com>
2011-03-25 16:21:35 +0800

    * env.sh fix.

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,6 +1,6 @@
 SCRIPT=$(readlink -f ${PWD}/${BASH_SOURCE})
 TOP_PATH=`dirname $SCRIPT`
 TOP_PATH="$TOP_PATH/.."
-export mg="$TOP_PATH/ENV/bin/python $TOP_PATH/lbforum_site/manage.py"
+export mg="$TOP_PATH/env/bin/python $TOP_PATH/lbforum_site/manage.py"
 
 
