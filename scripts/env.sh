bb1809d70a1c62d70b7476f49d8535006991cfda
vicalloy <zbirder@gmail.com>
2011-03-25 16:11:44 +0800

    * fix env.sh

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,2 +1,6 @@
-source ../env/bin/activate
-cd ../lbforum_site/
+SCRIPT=$(readlink -f ${PWD}/${BASH_SOURCE})
+TOP_PATH=`dirname $SCRIPT`
+TOP_PATH="$TOP_PATH/.."
+export mg="$TOP_PATH/ENV/bin/python $TOP_PATH/lbforum_site/manage.py"
+
+
