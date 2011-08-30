d73f2d471b27e638b3669c1775c2db08de72d05c
vicalloy <zbirder@gmail.com>
2011-08-30 22:10:39 +0800

    * fix settings.py

--- lbforum_site/settings.py
+++ lbforum_site/settings.py
@@ -53,7 +53,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(HERE, 'static/')
+STATIC_ROOT = os.path.join(HERE, 'collectedstatic')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -66,6 +66,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    os.path.join(HERE, 'static/'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
